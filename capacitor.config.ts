2e07ed9c243553dcd60adbf624a335f21008dc8f
Marc Ariza Barrios <marcariza2@gmail.com>
2023-04-19 19:06:08 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'made_in_sleep',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
